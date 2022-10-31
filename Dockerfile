41e66eacaa901b44fe9d5d28f4d758c7dcbd5054
rinriN <2210368@ecc.ac.jp>
2022-10-31 21:42:36 +0900

    :memo: : add auto reply message function

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.7.15-alpine
+FROM python:3.7.15
 
 WORKDIR /app
 
@@ -8,5 +8,6 @@ RUN /usr/local/bin/python -m pip install --upgrade pip
 RUN pip install Flask
 RUN pip install flask_cors
 RUN pip install line-bot-sdk
+RUN pip install requests-oauthlib
 
-CMD ["python", "app.py"]
+CMD ["python3", "app.py"]
--- app.py
+++ app.py
@@ -7,13 +7,12 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
+    MessageEvent, TextMessage, TextSendMessage, StickerSendMessage
 )
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi(
-    'BzInYuQWZ2KDpjYaRX+nGGk092AQ7UgWHkRx7IT8J8Xc7mbP6gxzDLgcLCuuePJW7FknCq6k/d8RHjxsLoviwUndZB2uzTOJgb6K/PBk3hKjBzSa4te7peTFaFTBmFg2KSFUZmv8o4I3dh2Tm2et3wdB04t89/1O/w1cDnyilFU=')
+line_bot_api = LineBotApi('BzInYuQWZ2KDpjYaRX+nGGk092AQ7UgWHkRx7IT8J8Xc7mbP6gxzDLgcLCuuePJW7FknCq6k/d8RHjxsLoviwUndZB2uzTOJgb6K/PBk3hKjBzSa4te7peTFaFTBmFg2KSFUZmv8o4I3dh2Tm2et3wdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('a842e0251982aac19ce2ffd563f28d3c')
 
 
@@ -25,7 +24,6 @@ def callback():
     # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
-
     # handle webhook body
     try:
         handler.handle(body, signature)
@@ -38,10 +36,26 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
+    # 預設回覆與使用者相同訊息
+    # line_bot_api.reply_message(
+    #     event.reply_token,
+    #     TextSendMessage(text=event.message.text))
+
+    # 根據接收文字回傳特定訊息
+    if event.message.text == '@info':
+        line_bot_api.reply_message(
+            event.reply_token,
+            [
+                TextSendMessage(text='hello'),
+                StickerSendMessage(package_id='1', sticker_id='2')
+            ]
+        )
+    else :
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text='何言ってんの？分がんないよ〜')
+        )
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5001, debug=True)
